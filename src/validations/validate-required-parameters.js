9234561db06db4a00eaa850c28a08a8b7aa1f604
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2020-04-10 18:08:27 +0200

    fix wrong validation

--- src/validations/validate-required-parameters.js
+++ src/validations/validate-required-parameters.js
@@ -1,21 +1,14 @@
-const getEmptyRequiredValues = (tools, requiredParameters) => {
-  return tools.inputs.filter(input => {
-    if(!requiredParameters.includes(input)) {
-      return false;
-    }
-
-    return input === undefined;
-  });
-};
-
 /**
  * Validate all required parameters are send to the action
  * @param {import('actions-toolkit').Toolkit} tools
  * @param {string[]} requiredParameters
  */
 module.exports = (tools, requiredParameters) => {
-  const emptyRequiredValues = getEmptyRequiredValues(tools, requiredParameters);
+  const emptyRequiredValues = requiredParameters.filter(requiredParameter => {
+    return tools.inputs[requiredParameter] === undefined;
+  });
+
   if (emptyRequiredValues.length) {
     tools.exit.failure(`You forgot to provide some required values [${emptyRequiredValues.join(', ')}]`)
   }
-}
+};
